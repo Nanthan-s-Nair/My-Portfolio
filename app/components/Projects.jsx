7f002166946017879fdfcc4eec1506e86b929524
Nanthan S Nair <143941884+Nanthan-s-Nair@users.noreply.github.com>
2024-04-08 23:35:49 +0530

    done7

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,5 +1,4 @@
-import Image from 'next/image';
-import Link from 'next/link';
+
 import React from 'react';
 import amazonimg from '../../public/images/skills/amazon.png';
 import elev from '../../public/images/skills/12A98EF4-FD75-45DE-A69C-AB76AD0BFD78_1_201_a.jpeg';
